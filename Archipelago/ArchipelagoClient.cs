c01312c722df61857bb617910a2a0fdc6704fe08
Crownmuri <jonkerssgh@gmail.com>
2026-07-26 11:23:00 +0900

    Hotfix version update

--- Archipelago/ArchipelagoClient.cs
+++ Archipelago/ArchipelagoClient.cs
@@ -19,7 +19,7 @@ namespace LaMulana2Archipelago.Archipelago
 {
     public class ArchipelagoClient
     {
-        public const string APVersion = "0.9.0";
+        public const string APVersion = "0.9.1";
         private const string Game = "La-Mulana 2";
 
         public static bool Authenticated;
